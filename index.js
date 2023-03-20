f304e0bc9262d09d95e7746bdb16c7d223de0fe9
Wakhungu N Rebecca <rebeccanakhanu@gmail.com>
2023-03-21 01:33:11 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+//identifying the variables
+let num1=31
+let num2=2
+let multiply=num1*num2
+//expected result=62
+//creating a random number
+const random = Math.ceil(Math.random() * 10);
+//modulars
+//defining the variables
+const num3=22;
+const num4=6;
+
+const mod=num3 % num4;
+//the highest number is 20
+ //an array of numbers is given below
+ const numbers=[4,8,12,16,20];
+ const max=Math.max( 20);
+ 
